3882862b88cb3750931ee088143104004706ff85
Qingyan <yangqingyan0@gmail.com>
2024-06-27 10:11:55 +0930

    feat: config radix ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@radix-ui/themes/styles.css';
 import App from './App';
+import { Theme, ThemePanel } from '@radix-ui/themes';
+
 // import firebase from '../FirebaseConfig';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Theme
+      accentColor='lime'
+      grayColor='sage'
+      panelBackground='translucent'
+      appearance='dark'
+      scaling='100%'
+      radius='full'
+    >
+      <App />
+      <ThemePanel />
+    </Theme>
   </React.StrictMode>,
 );
